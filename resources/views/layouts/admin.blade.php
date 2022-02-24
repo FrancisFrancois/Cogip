0553b9c7fff2d6deee1070f76fdd10f22f1f03c2
Francis <francisfrancois9189@gmail.com>
2022-02-24 15:48:39 +0100

    first commit

---
+++ resources/views/layouts/admin.blade.php
@@ -0,0 +1,213 @@
+@include('partials.head')
+<body>
+    <div>
+        <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js" defer></script>
+        
+        <div x-data="{ sidebarOpen: false }" class="flex h-screen bg-gray-200">
+            <div :class="sidebarOpen ? 'block' : 'hidden'" @click="sidebarOpen = false" class="fixed z-20 inset-0 bg-black opacity-50 transition-opacity lg:hidden"></div>
+        
+            @include('partials.navbar')
+
+            <main class="flex-1 overflow-x-hidden overflow-y-auto bg-gray-200">
+                <div class="container mx-auto px-6 py-8">
+                    <h3 class="px-6 py-3 border-b border-gray-200 bg-gray-700 text-center text-3xl leading-4 font-medium text-white uppercase tracking-wider sm:rounded-lg">Admin</h3>
+                        <br><br>
+                        <div class="container mx-auto bg-white rounded-lg shadow-lg">
+                          <div class="md:h-20 py-20 md:py-0 flex justify-center items-center">
+                            <div class="md:space-x-20 space-y-10 md:space-y-0">
+                              <a href="/newinvoice" class="py-3 px-6 text-white rounded-lg bg-gray-700 shadow-lg block md:inline-block hover:bg-gray-500">New Invoice</a>
+                              <a href="/newcontact" class="py-3 px-6 text-white rounded-lg bg-gray-700 shadow-lg block md:inline-block hover:bg-gray-500">New Contact</a>
+                              <a href="/newcompany" class="py-3 px-6 text-white rounded-lg bg-gray-700 shadow-lg block md:inline-block hover:bg-gray-500">New Company</a>
+                            </div>
+                          </div>
+                        
+                      </div>
+                    <div class="flex flex-col mt-8">
+                        <div class="-my-2 py-2 overflow-x-auto sm:-mx-6 sm:px-6 lg:-mx-8 lg:px-8">
+                            <div
+                                class="align-middle inline-block min-w-full shadow overflow-hidden sm:rounded-lg border-b border-gray-200">
+                                <table class="min-w-full">
+                                    <thead>
+                                        <tr>
+                                            <h4 class="px-6 py-3 border-b border-gray-200 bg-gray-700 text-left text-xs leading-4 font-medium text-white uppercase tracking-wider">Last Invoices</h4>
+                                            <th
+                                                class="px-6 py-3 border-b border-gray-200 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
+                                                Invoice Number</th>
+                                            <th
+                                                class="px-6 py-3 border-b border-gray-200 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
+                                                Dates</th>
+                                            <th
+                                                class="px-6 py-3 border-b border-gray-200 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
+                                                Company</th>
+                                            <th
+                                                class="px-6 py-3 border-b border-gray-200 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
+                                                </th>
+                                            <th class="px-6 py-3 border-b border-gray-200 bg-gray-50"></th>
+                                        </tr>
+                                    </thead>
+    
+                                    <tbody class="bg-white">
+                                        <tr>
+                                            <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
+                                                <div class="flex items-center">
+                                                    <div class="ml-4">
+                                                       
+                                                        <div class="text-sm leading-5 text-gray-500">F400-330</div>
+                                                    </div>
+                                                </div>
+                                            </td>
+    
+                                            <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
+                                           
+                                                <div class="text-sm leading-5 text-gray-500">23/02/2022</div>
+                                            </td>
+    
+                                            <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
+                                                <span
+                                                    class="text-sm leading-5 text-gray-500">Tesla</span>
+                                            </td>
+    
+                                            <td
+                                                class="px-6 py-4 whitespace-no-wrap border-b border-gray-200 text-sm leading-5 text-gray-500">
+                                                </td>
+    
+                                            <td
+                                                class="px-6 py-4 whitespace-no-wrap text-right border-b border-gray-200 text-sm leading-5 font-medium">
+                                                <a href="#" class="text-indigo-600 hover:text-indigo-900"></a>
+                                            </td>
+                                
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="flex flex-col mt-8">
+                        
+                        <div class="-my-2 py-2 overflow-x-auto sm:-mx-6 sm:px-6 lg:-mx-8 lg:px-8">
+                            <div
+                                class="align-middle inline-block min-w-full shadow overflow-hidden sm:rounded-lg border-b border-gray-200">
+                                <table class="min-w-full">
+                                    <thead>
+                                        <tr>
+                                            <h4 class="px-6 py-3 border-b border-gray-200 bg-gray-700 text-left text-xs leading-4 font-medium text-white uppercase tracking-wider">Last Contacts</h4>
+                                            <th
+                                                class="px-6 py-3 border-b border-gray-200 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
+                                                Name</th>
+                                            <th
+                                                class="px-6 py-3 border-b border-gray-200 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
+                                                Phone</th>
+                                            <th
+                                                class="px-6 py-3 border-b border-gray-200 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
+                                                E-mail</th>
+                                            <th
+                                                class="px-6 py-3 border-b border-gray-200 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
+                                                Company</th>
+                                            <th class="px-6 py-3 border-b border-gray-200 bg-gray-50"></th>
+                                        </tr>
+                                    </thead>
+    
+                                    <tbody class="bg-white">
+                                        <tr>
+                                            <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
+                                                <div class="flex items-center">
+                                                    <div class="ml-4">
+                                                   
+                                                        <div class="text-sm leading-5 text-gray-500">Jeff Bezos</div>
+                                                    </div>
+                                                </div>
+                                            </td>
+    
+                                            <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
+                                           
+                                                <div class="text-sm leading-5 text-gray-500">0472.59.29.80</div>
+                                            </td>
+    
+                                            <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
+                                                <span
+                                                    class="text-sm leading-5 text-gray-500">francisfrancois9189@gmail.com</span>
+                                            </td>
+    
+                                            <td
+                                                class="px-6 py-4 whitespace-no-wrap border-b border-gray-200 text-sm leading-5 text-gray-500">
+                                                Amazon</td>
+    
+                                            <td
+                                                class="px-6 py-4 whitespace-no-wrap text-right border-b border-gray-200 text-sm leading-5 font-medium">
+                                                <a href="#" class="text-indigo-600 hover:text-indigo-900"></a>
+                                            </td>
+                                
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="flex flex-col mt-8">
+                        
+                        <div class="-my-2 py-2 overflow-x-auto sm:-mx-6 sm:px-6 lg:-mx-8 lg:px-8">
+                            <div
+                                class="align-middle inline-block min-w-full shadow overflow-hidden sm:rounded-lg border-b border-gray-200">
+                                <table class="min-w-full">
+                                    <thead>
+                                        <tr>
+                                            <h4 class="px-6 py-3 border-b border-gray-200 bg-gray-700 text-left text-xs leading-4 font-medium text-white uppercase tracking-wider">Last Companies</h4>
+                                            <th
+                                                class="px-6 py-3 border-b border-gray-200 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
+                                                Name</th>
+                                            <th
+                                                class="px-6 py-3 border-b border-gray-200 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
+                                                TVA</th>
+                                            <th
+                                                class="px-6 py-3 border-b border-gray-200 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
+                                                Country</th>
+                                            <th
+                                                class="px-6 py-3 border-b border-gray-200 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
+                                                Type</th>
+                                            <th class="px-6 py-3 border-b border-gray-200 bg-gray-50"></th>
+                                        </tr>
+                                    </thead>
+    
+                                    <tbody class="bg-white">
+                                        <tr>
+                                            <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
+                                                <div class="flex items-center">
+                                                    <div class="ml-4">
+        
+                                                        <div class="text-sm leading-5 text-gray-500">Elon Musk</div>
+                                                    </div>
+                                                </div>
+                                            </td>
+    
+                                            <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
+                                            
+                                                <div class="text-sm leading-5 text-gray-500">BE0999999999</div>
+                                            </td>
+    
+                                            <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
+                                                <span
+                                                    class="text-sm leading-5 text-gray-500">USA</span>
+                                            </td>
+    
+                                            <td
+                                                class="px-6 py-4 whitespace-no-wrap border-b border-gray-200 text-sm leading-5 text-gray-500">
+                                                Supplier</td>
+    
+                                            <td
+                                                class="px-6 py-4 whitespace-no-wrap text-right border-b border-gray-200 text-sm leading-5 font-medium">
+                                                <a href="#" class="text-indigo-600 hover:text-indigo-900"></a>
+                                            </td>
+                                
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </main>
+        </div>
+    </div>
+</div>
+</body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,60 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('layouts.app');
+});
+
+Route::get('/companies', function () {
+    return view('layouts.companies');
+});
+
+Route::get('/admin', function () {
+    return view('layouts.admin');
+});
+
+Route::get('/invoices', function () {
+    return view('layouts.invoices');
+});
+
+Route::get('/contacts', function () {
+    return view('layouts.contacts');
+});
+
+Route::get('/newinvoice', function () {
+    return view('layouts.newinvoice');
+});
+
+Route::get('/newcontact', function () {
+    return view('layouts.newcontact');
+});
+
+Route::get('/newcompany', function () {
+    return view('layouts.newcompany');
+});
+
+Route::get('/detailcontact', function () {
+    return view('layouts.detailcontact');
+});
+
+
+
+
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';

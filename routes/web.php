5499c971b5d90164c4c13f56891a39564266a80a
Francis <francisfrancois9189@gmail.com>
2022-03-22 09:33:26 +0100

    MiddlewareAdminEditor

--- routes/web.php
+++ routes/web.php
@@ -19,90 +19,101 @@ use App\Http\Controllers\InvoiceController;
 |
 */
 
-
 Route::get('/', [AppController::class, 'index']);
 
-Route::get('/admin', [AdminController::class, 'index']);
 
 
+Route::middleware((['auth', 'role:admin']))->group(function () {
 
-Route::get('/companies', [CompanyController::class, 'index']);
+    Route::get('/admin', [AdminController::class, 'index']);
 
-Route::get('/detailcompany/{id}', [CompanyController::class, 'show'])
-    ->where(['id' => '[0-9]+']);
+    Route::get('/companies', [CompanyController::class, 'index']);
 
-Route::get('/newcompany', [CompanyController::class, 'create']);
+    Route::get('/detailcompany/{id}', [CompanyController::class, 'show'])
+        ->where(['id' => '[0-9]+']);
 
-Route::post('/newcompany', [CompanyController::class, 'store']);
+    Route::get('/newcompany', [CompanyController::class, 'create']);
 
-Route::get('/editcompany/{id}', [CompanyController::class, 'edit'])
-    ->where(['id' => '[0-9]+']);
+    Route::post('/newcompany', [CompanyController::class, 'store']);
 
-Route::put('/editcompany/update/{id}', [CompanyController::class, 'update'])
-    ->where(['id' => '[0-9]+']);
+    Route::get('/editcompany/{id}', [CompanyController::class, 'edit'])
+        ->where(['id' => '[0-9]+']);
 
-Route::delete('/editcompany/delete/{id}', [CompanyController::class, 'destroy'])
-    ->where(['id' => '[0-9]+']);
+    Route::put('/editcompany/update/{id}', [CompanyController::class, 'update'])
+        ->where(['id' => '[0-9]+']);
 
+    Route::delete('/editcompany/delete/{id}', [CompanyController::class, 'destroy'])
+        ->where(['id' => '[0-9]+']);
 
+    Route::get('/contacts', [ContactController::class, 'index']);
 
+    Route::get('/detailcontact/{id}', [ContactController::class, 'show'])
+        ->where(['id' => '[0-9]+']);
 
-Route::get('/contacts', [ContactController::class, 'index']);
+    Route::get('/newcontact', [ContactController::class, 'create']);
 
-Route::get('/detailcontact/{id}', [ContactController::class, 'show'])
-    ->where(['id' => '[0-9]+']);
+    Route::post('/newcontact', [ContactController::class, 'store']);
 
-Route::get('/newcontact', [ContactController::class, 'create']);
+    Route::get('/editcontact/{id}', [ContactController::class, 'edit'])
+        ->where(['id' => '[0-9]+']);
 
-Route::post('/newcontact', [ContactController::class, 'store']);
+    Route::put('/editcontact/update/{id}', [ContactController::class, 'update'])
+        ->where(['id' => '[0-9]+']);
 
-Route::get('/editcontact/{id}', [ContactController::class, 'edit'])
-    ->where(['id' => '[0-9]+']);
+    Route::delete('/editcontact/delete/{id}', [ContactController::class, 'destroy'])
+        ->where(['id' => '[0-9]+']);
 
-Route::put('/editcontact/update/{id}', [ContactController::class, 'update'])
-    ->where(['id' => '[0-9]+']);
+    Route::get('/invoices', [InvoiceController::class, 'index']);
 
-Route::delete('/editcontact/delete/{id}', [ContactController::class, 'destroy'])
-    ->where(['id' => '[0-9]+']);
+    Route::get('/detailinvoice/{id}', [InvoiceController::class, 'show'])
+        ->where(['id' => '[0-9]+']);
 
+    Route::get('/newinvoice', [InvoiceController::class, 'create']);
 
+    Route::post('/newinvoice', [InvoiceController::class, 'store']);
 
-Route::get('/invoices', [InvoiceController::class, 'index']);
+    Route::get('/editinvoice/{id}', [InvoiceController::class, 'edit'])
+        ->where(['id' => '[0-9]+']);
 
-Route::get('/detailinvoice/{id}', [InvoiceController::class, 'show'])
-    ->where(['id' => '[0-9]+']);
+    Route::put('/editinvoice/update/{id}', [InvoiceController::class, 'update'])
+        ->where(['id' => '[0-9]+']);
 
-Route::get('/newinvoice', [InvoiceController::class, 'create']);
-
-Route::post('/newinvoice', [InvoiceController::class, 'store']);
-
-Route::get('/editinvoice/{id}', [InvoiceController::class, 'edit'])
-    ->where(['id' => '[0-9]+']);
-
-Route::put('/editinvoice/update/{id}', [InvoiceController::class, 'update'])
-    ->where(['id' => '[0-9]+']);
-
-Route::delete('/editinvoice/delete/{id}', [InvoiceController::class, 'destroy'])
-    ->where(['id' => '[0-9]+']);
-
+    Route::delete('/editinvoice/delete/{id}', [InvoiceController::class, 'destroy'])
+        ->where(['id' => '[0-9]+']);
+});
 
 
+Route::middleware((['auth', 'role:editor']))->group(function () {
 
+    Route::get('/admin', [AdminController::class, 'index']);
 
+    Route::get('/companies', [CompanyController::class, 'index']);
 
+    Route::get('/detailcompany/{id}', [CompanyController::class, 'show'])
+        ->where(['id' => '[0-9]+']);
 
+    Route::get('/newcompany', [CompanyController::class, 'create']);
 
+    Route::post('/newcompany', [CompanyController::class, 'store']);
 
+    Route::get('/contacts', [ContactController::class, 'index']);
 
+    Route::get('/detailcontact/{id}', [ContactController::class, 'show'])
+        ->where(['id' => '[0-9]+']);
 
+    Route::get('/newcontact', [ContactController::class, 'create']);
 
+    Route::post('/newcontact', [ContactController::class, 'store']);
 
+    Route::get('/invoices', [InvoiceController::class, 'index']);
 
+    Route::get('/detailinvoice/{id}', [InvoiceController::class, 'show'])
+        ->where(['id' => '[0-9]+']);
 
+    Route::get('/newinvoice', [InvoiceController::class, 'create']);
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
+    Route::post('/newinvoice', [InvoiceController::class, 'store']);
+});
 
 
 
